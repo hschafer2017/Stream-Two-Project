10d51160a5dc6f8869fc4f743e98ab62c0d02b4c
Haley Schafer <hschafer@umich.edu>
2018-05-09 13:49:39 +0000

    working game function using data attributes instead of function initiated buttons

--- index.js
+++ index.js
@@ -1,136 +1,127 @@
+// function flash(b) {
+//     if (bopPrompt() == "Bop It") {
+//         document.getElementById("bops").innerHTML =
+//             "Bop It!"
+//         $("h3").removeClass("bops");
+//         console.log("bopping")
+//     }
+//     if (bopPrompt() == "Twist It") {
+//         document.getElementById("twists").innerHTML =
+//             "Twist It!"
+//         $("h3").removeClass("twists");
+//         console.log("twisting")
+//     }
+//     if (bopPrompt() == "Pull It") {
+//         document.getElementById("pulls").innerHTML =
+//             "Pull It!"
+//         $("h3").removeClass("pulls");
+//         console.log("pulling")
+//     }
+// }
+// // console.log(flash())
+
+
+//     // if ($("#bop").attr("src") == "bop1.jpg") {
+//     //     $("#bop").attr("src", "bop2.jpg");
+
+//     // if ($("#bop").attr("src") == "pull1.jpg") {
+//     //     $("#bop").attr("src", "pull2.jpg");
+
+//     // if ($("#bop").attr("src") == "twist1.jpg") {
+//     //     $("#bop").attr("src", "twist2.jpg");
+
+// function startGame() {
+//         if (command == bopButton) {
+//             // $("#bop").attr("src, bop1.jpg").fade;
+//             console.log("correct");
+//             // document.getElementById("bops").classList.add("bops");
+//         }
+//         else {
+//             console.log("You Lose")
+//         }
+//     
+//         if (command == pullButton) {
+//             // $("#pull").attr("src, pull1.jpg").fade;
+//             console.log("Correct");
+//             // document.getElementById("twists").classList.add("twists");
+//         }
+//         else {
+//             console.log("You Lose")
+//         }
+//     
+//         if (command == twistButton) {
+//             // $("#twist").attr("src, twist1.jpg").fade;
+//             console.log("Correct");
+//             // document.getElementById("pulls").classList.add("pulls");
+//             // console.log("done pulling");
+//            
+//         }
+//         else {
+//             console.log("You lost!");
+//             // document.getElementById("losing").innerHTML =
+//             //     "You Lost!";
+//             // $("h3").removeClass("losing");
+//             // stop loop
+//         }
+// }
+
 let commands = ["Bop It", "Twist It", "Pull It"];
 
 function bopPrompt(a) {
 
     return commands[Math.floor(Math.random() * commands.length)];
 }
-console.log(bopPrompt());
-// Remember to credit Stack overflow for Randomizer 
 
-function flash(b) {
-    if (bopPrompt() == "Bop It") {
-        document.getElementById("bops").innerHTML =
-            "Bop It!"
-        $("h3").removeClass("bops");
-        console.log("bopping")
-    }
-    else if (bopPrompt() == "Twist It") {
-        document.getElementById("twists").innerHTML =
-            "Twist It!"
-        $("h3").removeClass("twists");
-        console.log("twisting")
-    }
-    else if (bopPrompt() == "Pull It") {
-        document.getElementById("pulls").innerHTML =
-            "Pull It!"
-        $("h3").removeClass("pulls");
-        console.log("pulling")
-
-    } else {
-        console.log("flash fail")
-    }
-}
-
-
-// console.log(bopPrompt());
-// console.log(typeof(bopPrompt()));
-
-
-function bopIt() {
-    // if ($("#bop").attr("src") == "bop1.jpg") {
-    //     $("#bop").attr("src", "bop2.jpg");
-        console.log('bop it');
-        return true; 
-    //     } else {
-    //         return false;
-    //     }
-
-    }
-
-
-function pullIt() {
-    // if ($("#pull").attr("src") == "pull1.jpg") {
-    //     $("#pull").attr("src", "pull2.jpg");
-        console.log('pull it');
-        return true;
-    // }
-    // else {
-    //     return false;
-    // }
-}
-
-function twistIt() {
-    // if ($("#twist").attr("src") == "twist1.jpg") {
-    //     $("#twist").attr("src", "twist2.jpg");
-        console.log('twist it');
-        return true;
-    // }
-    // else {
-    //     return false;
-    // }
-}
-// Remember to credit Antonjia for the return/true/false in if statements
 function startGame() {
-flash();
-    // bopPrompt();
-    for (let i = 0; i <= 5; i++) {
-
-        // if (bopPrompt() == "Bop It") {
-        //     alert("bopit")
-        // }
-
-        // if (bopPrompt() == "Twist It") {
-        //     alert("twistit")
-        // }
-
-        // if (bopPrompt() == "Pull It") {
-        //     alert("pullit")
-        // }
-
-
-        if (bopPrompt() == "Bop It" && bopIt()) {
-            // $("#bop").attr("src, bop1.jpg").fade;
-            console.log("bop");
-            if (i <= 500) {
-                continue;
-            }
-            else {
-                break;
-
-            }
-            // continue loop & credit stack overflow for continue/break part
-    }
-        else if (bopPrompt() == "Twist It" && twistIt()) {
-            // $("#pull").attr("src, pull1.jpg").fade;
-            console.log("twist");
-            if (i <= 500) {
-                continue;
-            }
-            else {
-                break;
-
-            }
-            // continue loop 
-    }
-        else if (bopPrompt() == "Pull It" && pullIt()) {
-            // $("#twist").attr("src, twist1.jpg").fade;
-            console.log("pull");
-            if (i <= 500) {
-                continue;
-            }
-            else {
-                break;
-            }
-            // continue loop 
-        }
-        else {
-            // i = 501;
-            break;
-            console.log("You lost!");
-            document.getElementById("losing").innerHTML =
-                "You Lost!";
-            $("h3").removeClass("losing");
-            // stop loop
+    
+    let command = bopPrompt();
+    console.log(command); 
+    let bopButton = document.getElementById('bopIt').getAttribute("data-type");
+    let pullButton = document.getElementById('pullIt').getAttribute("data-type");
+    let twistButton = document.getElementById('twistIt').getAttribute("data-type");
+    
+    document.getElementById('bopIt').onclick = function() {
+        if (command == bopButton) {
+            console.log("Correct Bop")
+            startGame()
+            
+        }else {
+            console.log("You Lose")
+            
         }
     }
+    
+    document.getElementById('pullIt').onclick = function() {
+        if (command == pullButton) {
+            console.log("Correct Pull")
+            startGame()
+            
+        }else {
+            console.log("You Lose")
+            
+        }
+    }
+    
+    document.getElementById('twistIt').onclick = function() {
+        if (command == twistButton) {
+            console.log("Correct Twist")
+            startGame()
+            
+        }else {
+            console.log("You Lose")
+            
+        }
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+// Remember to credit Stack overflow for Randomizer
+// using data-type is much easier than using a function within the button 
